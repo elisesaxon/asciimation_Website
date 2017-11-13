6363a9d3654e5d3ed600f47cacc0491451dc04ce
elisesaxon <elisesaxon@gmail.com>
2017-11-12 21:18:48 -0800

    Set up javascript functions and verify that they work on events

---
+++ ascii.js
@@ -0,0 +1,23 @@
+function start() {
+	alert("TIME TO START");
+}
+
+function stop() {
+	alert("BETTER STOP");
+}
+
+function changeAnimation(choice) {
+	alert("ANNNNNNIMATE: " + choice);
+}
+
+function changeFontSize(newSize) {
+	alert("change siiiiize: " + newSize);
+}
+
+function speedChange(box) {
+	if(box.checked) {
+		//make it turbo (50 ms delay)
+	} else {
+		//make it normal (250 ms delay)
+	}
+}
