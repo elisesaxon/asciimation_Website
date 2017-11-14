54c328b20cfc365344344d969e9246cce9228bbb
elisesaxon <elisesaxon@gmail.com>
2017-11-13 22:10:39 -0800

    Implement start() and stop() functions in js

--- ascii.js
+++ ascii.js
@@ -1,23 +1,39 @@
+var loopingvar;
+var sceneidx = -1;
+
 function start() {
-	alert("TIME TO START");
+	var scenes = document.getElementById("viewport").value.split("=====\n");
+    loopingvar = setInterval(loop, 250, scenes);
+}
+
+function loop(scenes) {
+	if(sceneidx < (scenes.length - 1)) {
+		sceneidx++;
+	} else {
+		sceneidx = 0;
+	}
+	
+    document.getElementById("viewport").value = scenes[sceneidx];
 }
 
 function stop() {
-	alert("BETTER STOP");
+	clearInterval(loopingvar);
 }
 
 function changeAnimation(choice) {
-	document.getElementById("viewport").value=ANIMATIONS[choice];
+	document.getElementById("viewport").value = ANIMATIONS[choice];
 }
 
 function changeFontSize(newSize) {
-	alert("change siiiiize: " + newSize);
+	document.getElementById("viewport").style.fontSize = newSize;
 }
 
 function speedChange(box) {
 	if(box.checked) {
 		//make it turbo (50 ms delay)
+		//setInterval("start()", 50);
 	} else {
 		//make it normal (250 ms delay)
+		//setInterval("start()", 250);
 	}
 }
