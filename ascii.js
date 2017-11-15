190f5ff97d59314acc7e9b7245357f1c92ab8123
elisesaxon <elisesaxon@gmail.com>
2017-11-14 22:33:36 -0800

    Enable and disable control elements at appropriate times

--- ascii.js
+++ ascii.js
@@ -5,12 +5,17 @@ var speed = 250;
 
 
 function start() {
+	document.getElementById("stopbutton").disabled = false;
+	document.getElementById("startbutton").disabled = true;
+	document.getElementById("animationmenu").disabled = true;
+
 	if(!scenes) {
 		scenes = document.getElementById("viewport").value.split("=====\n");
 	}
 	
     loopingvar = setInterval(loop, speed);
 }
+
 
 function loop() {
 	if(sceneidx < (scenes.length - 1)) {
@@ -22,10 +27,16 @@ function loop() {
     document.getElementById("viewport").value = scenes[sceneidx];
 }
 
+
 function stop() {
+	document.getElementById("startbutton").disabled = false;
+	document.getElementById("animationmenu").disabled = false;
+	document.getElementById("stopbutton").disabled = true;
+	
     clearInterval(loopingvar);
     loopingvar = 0;
 }
+
 
 function changeAnimation(choice) {
 	document.getElementById("viewport").value = ANIMATIONS[choice];
@@ -33,9 +44,11 @@ function changeAnimation(choice) {
 	scenes = 0;
 }
 
+
 function changeFontSize(newSize) {
 	document.getElementById("viewport").style.fontSize = newSize;
 }
+
 
 function speedChange(box) {
 	if(box.checked) {
